3760ce1a6ffbb6cd91597cf08330367fdce6756b
경호 양 <chchch1213@naver.com>
2021-04-16 23:53:18 +0900

    test

---
+++ Client/Forms/FormMain.cs
@@ -0,0 +1,27 @@
+﻿using Client.Classes;
+using MetroFramework.Forms;
+using System.Windows.Forms;
+
+namespace Client.Forms
+{
+    public partial class FormMain : MetroForm
+    {
+        Processor proc = Processor.Instance;
+        public Employee MyEmp;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            proc.formMain = this;
+            (new FormLogin()).ShowDialog();
+        }
+        private void FormMain_Shown(object sender, System.EventArgs e)
+        {
+
+        }
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            proc.Close();
+        }
+    }
+}
