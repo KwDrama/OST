993f8257a1cefa2d4747f39d4073a90f7d901f41
Hong Myoung Jun <ghdqkxk159@naver.com>
2021-05-25 10:58:43 +0900

    build AES256 function

--- OSTLibrary/Securities/AES256.cs
+++ OSTLibrary/Securities/AES256.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Text;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace OSTLibrary.Securities
 {
@@ -7,14 +9,66 @@ namespace OSTLibrary.Securities
     {
         public static byte[] Encrypt(byte[] data, string key)
         {
-            return data;
+            SHA256Managed sha256Managed = new SHA256Managed();
+            RijndaelManaged aes = new RijndaelManaged();
+
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            // salt = 비밀번호의 길이를 sha256 해쉬값
+            var salt = sha256Managed.ComputeHash(Encoding.UTF8.GetBytes(key.Length.ToString()));
+
+            // PBKDF2(Key - Based Key Derivation Function)
+            // 65525번 반복
+            var PBKDF2Key = new Rfc2898DeriveBytes(key, salt, 65535, HashAlgorithmName.SHA256);
+            var secretKey = PBKDF2Key.GetBytes(aes.KeySize / 8);
+            var iv = PBKDF2Key.GetBytes(aes.BlockSize / 8);
+
+            byte[] xBuff = null;
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, aes.CreateEncryptor(secretKey, iv), CryptoStreamMode.Write))
+                {
+                    cs.Write(data, 0, data.Length);
+                }
+                xBuff = ms.ToArray();
+            }
+            return xBuff;
         }
         public static string Encrypt(string data, string key)
             => Encoding.Default.GetString(Encrypt(Encoding.Default.GetBytes(data), key));
 
         public static byte[] Decrypt(byte[] data, string key)
         {
-            return data;
+            SHA256Managed sha256Managed = new SHA256Managed();
+            RijndaelManaged aes = new RijndaelManaged();
+
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            // salt = 비밀번호의 길이를 sha256 해쉬값
+            var salt = sha256Managed.ComputeHash(Encoding.UTF8.GetBytes(key.Length.ToString()));
+
+            // PBKDF2(Key - Based Key Derivation Function)
+            // 65525번 반복
+            var PBKDF2Key = new Rfc2898DeriveBytes(key, salt, 65535, HashAlgorithmName.SHA256);
+            var secretKey = PBKDF2Key.GetBytes(aes.KeySize / 8);
+            var iv = PBKDF2Key.GetBytes(aes.BlockSize / 8);
+
+            byte[] xBuff = null;
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, aes.CreateDecryptor(secretKey, iv), CryptoStreamMode.Write))
+                {
+                    cs.Write(data, 0, data.Length);
+                }
+                xBuff = ms.ToArray();
+            }
+            return xBuff;
         }
         public static string Decrypt(string data, string key)
             => Encoding.Default.GetString(Decrypt(Encoding.Default.GetBytes(data), key));
