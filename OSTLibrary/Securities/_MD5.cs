4891379d47db610da5e4f79ac559115cb87d7ff6
Hong Myoung Jun <ghdqkxk159@naver.com>
2021-05-25 11:18:56 +0900

    build _MD5 function

---
+++ OSTLibrary/Securities/_MD5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace OSTLibrary.Securities
+{
+    public static class _MD5
+    {
+        public static string Encrypt(string data)
+        {
+            var md5Hash = MD5.Create();
+            byte[] temp = md5Hash.ComputeHash(Encoding.Default.GetBytes(data));
+            StringBuilder hdata = new StringBuilder();
+
+            for (int i = 0; i < temp.Length; i++)
+                hdata.Append(temp[i].ToString("x2"));
+
+            return hdata.ToString();
+        }
+    }
+}
